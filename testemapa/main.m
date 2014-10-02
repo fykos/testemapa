574dc5080b84e9d1d4c9217b26f601e0388adc8d
CasaDaArvore <bruno@iMac-de-Bruno.local>
2014-10-02 17:14:05 -0300

    Initial Commit

---
+++ testemapa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testemapa
+//
+//  Created by CasaDaArvore on 02/10/14.
+//  Copyright (c) 2014 Elis Nunes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ testemapaTests/testemapaTests.m
@@ -0,0 +1,40 @@
+//
+//  testemapaTests.m
+//  testemapaTests
+//
+//  Created by CasaDaArvore on 02/10/14.
+//  Copyright (c) 2014 Elis Nunes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface testemapaTests : XCTestCase
+
+@end
+
+@implementation testemapaTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
